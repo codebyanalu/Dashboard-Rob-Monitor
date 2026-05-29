31024d7ec6e20c34b3be10cd87b6d1850bebe2aa
Ana Luiza <anasofia.neressoares@gmail.com>
2026-05-29 15:03:26 -0300

    feat: 28/05

--- data/2026/may/week-4.js
+++ data/2026/may/week-4.js
@@ -35,10 +35,10 @@ var DATA4 = {
     maas: { monitor: 17, prenota: 9, robo: 11, total: 26, responsavel1: { monitor: 6, prenota: 1, robo: 3, total: 7 }, responsavel2: { monitor: 11, prenota: 8, robo: 8, total: 19 } }
   },
   d4: {
-    hp: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_recanto: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_samambaia: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    maas: { monitor: 0, prenota: 0, robo: 0, total: 0, responsavel1: { monitor: 0, prenota: 0, robo: 0, total: 0 }, responsavel2: { monitor: 0, prenota: 0, robo: 0, total: 0 } }
+    hp: { monitor: 27, prenota: 4, robo: 17, total: 31 },
+    urbi_recanto: { monitor: 8, prenota: 2, robo: 7, total: 10 },
+    urbi_samambaia: { monitor: 14, prenota: 9, robo: 13, total: 23 },
+    maas: { monitor: 15, prenota: 7, robo: 10, total: 22, responsavel1: { monitor: 7, prenota: 4, robo: 3, total: 11 }, responsavel2: { monitor: 8, prenota: 3, robo: 7, total: 11 } }
   },
   d5: {
     hp: { monitor: 0, prenota: 0, robo: 0, total: 0 },
