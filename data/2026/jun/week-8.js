d92d060d26bd6d5332c10eef75640f4f693fa586
Ana Luiza <anasofia.neressoares@gmail.com>
2026-06-29 10:56:48 -0300

    feat: inclusão 24, 25, 26/06

--- data/2026/jun/week-8.js
+++ data/2026/jun/week-8.js
@@ -29,21 +29,21 @@ var DATA8 = {
     maas: { monitor: 13, prenota: 12, robo: 10, total: 25, responsavel1: { monitor: 6, prenota: 3, robo: 6, total: 9 }, responsavel2: { monitor: 7, prenota: 9, robo: 4, total: 16 }}
   },
   d3: {
-    hp: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_recanto: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_samambaia: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    maas: { monitor: 0, prenota: 0, robo: 0, total: 0, responsavel1: { monitor: 0, prenota: 0, robo: 0, total: 0 }, responsavel2: { monitor: 0, prenota: 0, robo: 0, total: 0 }}
+    hp: { monitor: 11, prenota: 2, robo: 7, total: 13 },
+    urbi_recanto: { monitor: 13, prenota: 4, robo: 12, total: 17 },
+    urbi_samambaia: { monitor: 21, prenota: 2, robo: 18, total: 23 },
+    maas: { monitor: 15, prenota: 8, robo: 12, total: 23, responsavel1: { monitor: 6, prenota: 5, robo: 6, total: 11 }, responsavel2: { monitor: 9, prenota: 3, robo: 6, total: 12 }}
   },
   d4: {
-    hp: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_recanto: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_samambaia: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    maas: { monitor: 0, prenota: 0, robo: 0, total: 0, responsavel1: { monitor: 0, prenota: 0, robo: 0, total: 0 }, responsavel2: { monitor: 0, prenota: 0, robo: 0, total: 0 }}
+    hp: { monitor: 19, prenota: 10, robo: 13, total: 29 },
+    urbi_recanto: { monitor: 11, prenota: 3, robo: 9, total: 14 },
+    urbi_samambaia: { monitor: 18, prenota: 2, robo: 15, total: 20 },
+    maas: { monitor: 19, prenota: 12, robo: 16, total: 31, responsavel1: { monitor: 13, prenota: 9, robo: 11, total: 22 }, responsavel2: { monitor: 6, prenota: 3, robo: 5, total: 9 }}
   },
   d5: {
-    hp: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_recanto: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    urbi_samambaia: { monitor: 0, prenota: 0, robo: 0, total: 0 },
-    maas: { monitor: 0, prenota: 0, robo: 0, total: 0, responsavel1: { monitor: 0, prenota: 0, robo: 0, total: 0 }, responsavel2: { monitor: 0, prenota: 0, robo: 0, total: 0 }}
+    hp: { monitor: 25, prenota: 3, robo: 17, total: 28 },
+    urbi_recanto: { monitor: 20, prenota: 3, robo: 18, total: 23 },
+    urbi_samambaia: { monitor: 6, prenota: 21, robo: 5, total: 27 },
+    maas: { monitor: 11, prenota: 7, robo: 11, total: 18, responsavel1: { monitor: 5, prenota: 0, robo: 5, total: 5 }, responsavel2: { monitor: 6, prenota: 7, robo: 6, total: 13 }}
   }
 };
